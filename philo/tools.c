515063b863908c45d399d31fa45e33daa0e98d72
Maxime Koyamba <mkoyamba@Maximes-iMac.local>
2022-09-03 17:07:30 +0200

    No leaks

--- philo/tools.c
+++ philo/tools.c
@@ -6,7 +6,7 @@
 /*   By: mkoyamba <mkoyamba@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 18:30:58 by mkoyamba          #+#    #+#             */
-/*   Updated: 2022/09/03 16:51:37 by mkoyamba         ###   ########.fr       */
+/*   Updated: 2022/09/03 17:07:17 by mkoyamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	all_free(t_data *data)
 	while (n < data->len)
 	{
 		pthread_mutex_destroy(data->mutex[n]);
+		free(data->mutex[n]);
+		free(data->thread[n]);
 		n++;
 	}
 	free(data->mutex);
